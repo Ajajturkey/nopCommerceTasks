9645dc6b163092bac3e995cbb3c3a89159143a98
Ahmad Ajaj Alkurdi <ajajturkey@gmail.com>
2018-06-05 11:53:10 +0300

    Creat 2 Tasks Requested
    Task1
    Adding Sql script to add new coulumn and update locals
    Task2
    Create new Widget that write on product page, can be confiqured from admin Area

---
+++ src/Plugins/Nop.Plugin.Widgets.ProductDescription/Components/WidgetsProductDescriptionViewComponent.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nop.Core;
+using Nop.Core.Caching;
+using Nop.Plugin.Widgets.ProductDescription.Models;
+using Nop.Services.Configuration;
+using Nop.Services.Media;
+using Nop.Web.Framework.Components;
+
+namespace Nop.Plugin.Widgets.ProductDescription.Components
+{
+    [ViewComponent(Name = "WidgetsProductDescription")]
+    public class WidgetsProductDescriptionViewComponent : NopViewComponent
+    {
+        private readonly IStoreContext _storeContext;
+        private readonly IStaticCacheManager _cacheManager;
+        private readonly ISettingService _settingService;
+        private readonly IPictureService _pictureService;
+
+        public WidgetsProductDescriptionViewComponent(IStoreContext storeContext, 
+            IStaticCacheManager cacheManager, 
+            ISettingService settingService, 
+            IPictureService pictureService)
+        {
+            this._storeContext = storeContext;
+            this._cacheManager = cacheManager;
+            this._settingService = settingService;
+        }
+
+        public IViewComponentResult Invoke(string widgetZone, object additionalData)
+        {
+            var productDescription = _settingService.LoadSetting<ProductDescriptionSettings>(_storeContext.CurrentStore.Id);
+
+            var model = new PublicInfoModel
+            {
+                Description = productDescription.Description
+            };
+
+            if (string.IsNullOrEmpty(model.Description))
+                //no text found
+                return Content("");
+
+            return View("~/Plugins/Widgets.ProductDescription/Views/PublicInfo.cshtml", model);
+        }
+
+    }
+}
---
+++ src/Plugins/Nop.Plugin.Widgets.ProductDescription/Models/ConfigurationModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Nop.Web.Framework.Mvc.ModelBinding;
+using Nop.Web.Framework.Mvc.Models;
+
+namespace Nop.Plugin.Widgets.ProductDescription.Models
+{
+    public class ConfigurationModel : BaseNopModel
+    {
+        public int ActiveStoreScopeConfiguration { get; set; }
+        
+        [NopResourceDisplayName("Plugins.Widgets.ProductDescription.Text")]
+        public string Description { get; set; }
+        public bool Description_OverrideForStore { get; set; }
+        
+    }
+}
